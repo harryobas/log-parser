fd4c71f7a85d0f0b215db5299d48fc875ddf16d9
harry obaseki <harryobas@gmail.com>
2020-01-18 06:29:08 +0100

    list of pages with most views ordered from highest to lowest

--- spec/log/parser/most_visited_pages_extractor_spec.rb
+++ spec/log/parser/most_visited_pages_extractor_spec.rb
@@ -27,7 +27,15 @@ RSpec.describe Log::Parser::MostVisitedPagesExtractor do
       expect(pages_extractor).not_to eq nil
       expect(pages_extractor).to be_a Log::Parser::MostVisitedPagesExtractor
       expect(pages_extractor.pages).to be_a Array
+      expect(pages_extractor.pages.empty?).to eq false
     end
+  end
+  describe "#most_viewed" do
+    it "returns a list of pages with most views ordered from highest to lowest" do
+      pages_extractor = Log::Parser::MostVisitedPagesExtractor.new(pages)
+      expect(pages_extractor.most_viewed).to eq [["/about", 5], ["/contact", 4], ["/help_page/1", 3], ["/home", 2]]
+    end
+    
   end
 
 end
