34482a02890720f862f4d0e074af277657692c1c
harry obaseki <harryobas@gmail.com>
2020-01-19 16:11:49 +0100

    refactoring

--- spec/log/parser/most_page_views_spec.rb
+++ spec/log/parser/most_page_views_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe Log::Parser::MostPageViews do
       visited_pages = Log::Parser::MostPageViews.new(pages)
       expect(visited_pages).not_to eq nil
       expect(visited_pages.pages).to be_a Array
-      expect(most_page_views.pages.empty?).to eq false
+      expect(visited_pages.pages.empty?).to eq false
     end
   end
 
--- spec/log/parser/parser_facade_spec.rb
+++ spec/log/parser/parser_facade_spec.rb
@@ -2,15 +2,6 @@ require "spec_helper"
 
 RSpec.describe Log::Parser::ParserFacade do
 
-  let :result_one do
-  [["/help_page/1", 5], ["/about", 5], ["/index", 4], ["/home", 1], ["/about/2", 1]]
-end
-
-let :result_two do
-  [["/help_page/1", 5], ["/about", 5], ["/index", 4], ["/home", 1], ["/about/2", 1]]
-
-end
-
   context "initalization" do
     it "initalizes with a file path" do
       file = File.expand_path("spec/fixtures/web.log")
@@ -29,11 +20,8 @@ end
     it "gets list of unique page views" do
       file = File.expand_path("spec/fixtures/web.log")
       parser_facade = Log::Parser::ParserFacade.new(file)
-      Log::Parser::UniquePageViews.any_instance.expects(:unique_visits).returns(result)
+      Log::Parser::UniquePageViews.any_instance.expects(:unique_visits)
       parser_facade.list_of_unique_page_views
-    end
-    it "#list_of_most_viewed_pages" do
-
     end
 
   end
